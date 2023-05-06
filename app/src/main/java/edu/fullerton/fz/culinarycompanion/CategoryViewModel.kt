ceb8c42d5a2bb910c848be8246aec54656365ef1
Zachary Thompson <zachthompson406@gmail.com>
2023-05-05 21:21:23 -0700

    Fix layout issues

--- app/src/main/java/edu/fullerton/fz/culinarycompanion/CategoryViewModel.kt
+++ app/src/main/java/edu/fullerton/fz/culinarycompanion/CategoryViewModel.kt
@@ -9,16 +9,4 @@ import edu.fullerton.fz.culinarycompanion.api.MealDBExecutor
 private const val LOG_TAG = "CategoryViewModel"
 class CategoryViewModel : ViewModel(){
     val categoryLiveData: LiveData<List<Category>> = MealDBExecutor().fetchCategories()
-
-    fun getCategories(): List<List<Category>>? {
-        Log.i(LOG_TAG, "Getting Categories")
-//        Thread.sleep(5000)
-        return if (this.categoryLiveData.value != null) {
-            Log.d(LOG_TAG, "Category List Size: ${categoryLiveData!!.value!!.size}")
-            val data = categoryLiveData.value!!.chunked(2) {it.take(2)}
-            data
-        } else {
-            null
-        }
-    }
 }
